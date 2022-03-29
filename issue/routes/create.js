9e2bed3a498a17221bc4c9f54b7096e3f2d03d5f
wldlsgur <dhgg321@naver.com>
2022-03-29 19:59:25 +0900

    select schoolmanagementRouter add

--- issue/routes/create.js
+++ issue/routes/create.js
@@ -25,19 +25,19 @@ router.post('/user', function(req, res){
 router.post('/schoolmanagement', function(req, res){
     let menu = req.body.menu;
     let school = req.body.school;
-    let group = req.body.group;
+    let room = req.body.room;
     let title = req.body.title;
     let content = req.body.content;
     let month = req.body.month;
     let year = req.body.year;
     let day = req.body.day;
-    console.log(menu, school, group, title, content, month, year, day);
-    if(!menu || !school || !group || !title || !content || !month || !year || !day){
+   
+    if(!menu || !school || !room || !title || !content || !month || !year || !day){
 	res.send("plz send require elements");
 	return;
     }
 
-    db.query(`INSERT INTO schoolmanagement VALUES('${menu}', '${school}', '${group}', '${title}', '${content}', '${month}', '${year}', '${day}')`, function(err, result){
+    db.query(`INSERT INTO schoolmanagement VALUES('${menu}', '${school}', '${room}', '${title}', '${content}', '${month}', '${year}', '${day}')`, function(err, result){
 	if(err){
 	    res.status(400).send(err);
 	    return;
