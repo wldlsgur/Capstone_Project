1d266e3d93e96721e274abcc030873829624744f
wldlsgur <dhgg321@naver.com>
2022-04-08 00:08:54 +0900

    select function

--- issue/function/query.js
+++ issue/function/query.js
@@ -11,13 +11,33 @@ module.exports = {
             if(i != data.length-1) make_query += ',';
         }
         make_query += ')';
-	    console.log(data);
-	    console.log(make_query);
         db.query(make_query, data, function(err, result){
             if(err){
                 return err;
             } 
         })
 	return this.response;
+    },
+    select : function(target, table, data, AND_OR, order){
+        let make_query = `SELECT`;
+        for(let i=0 ; i<target.length ; i++){
+            make_query += target[i];
+            if(i != target.length-1) make_query += ',';
+        }
+        make_query += `FROM ${table} WHERE `;
+
+        for(let i=0; i<data.length ; i++){
+            make_query += `${data[i]}=?` ;
+            if(i != data.length-1) make_query += AND_OR;
+        }
+        console.log(make_query);
+
+        db.query(make_query, data, function(err, result){
+            if(err){
+                return err;
+            }
+            if(order === 'json') return result[0];
+            else if(order === 'json_array') return result;
+        })
     }
 }
