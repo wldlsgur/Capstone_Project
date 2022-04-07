eb8483b6e06006c37eba1038d97d85521802fa53
wldlsgur <dhgg321@naver.com>
2022-04-08 00:09:09 +0900

    asdf

--- issue/routes/parentinfo.js
+++ issue/routes/parentinfo.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const router = express();
 const db = require('../DB/db');
+const db_query = require('../function/query');
 
 router.get('/info', function(req, res){
-    let id = req.query.id;
+	let data_array = [
+		req.query.id
+	];
 
-    if(!id){
+    if(!data[0]){
         res.send('plz send require elements'); 
         return;
     }
-
-    let query = `SELECT * FROM parentinfo WHERE id='${id}'`;
-    db.query(query, function(err, result){
-        if(err){
-            res.status(400).send(err);
-            return;
-        }
-        res.send(result);
-    })
+	res.send(db_query.select('*', 'parentinfo', data_array, '', 'json_array'));
 })
 
 router.get('/room/allinfo', function(req, res){
