7781e57acf60318530b5275af50f1aff939c9d84
인혁 지 <dhgg321@naver.com>
2022-04-12 23:56:13 +0900

    사진test3

--- issue/routes/uploadimage.js
+++ issue/routes/uploadimage.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../DB/db');
 
-router.post('/:target/:key/:value1/value2', function(req, res){
+router.post('/:target/:key/:value1/:value2', function(req, res){
 	let file = req.file;
 	let target = req.params.target;
 	let key = req.params.key;
