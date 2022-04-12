906bbe58ac5104678f960496e8859f1d1dbed48c
wldlsgur <dhgg321@naver.com>
2022-04-13 01:13:07 +0900

    진짜 성공하자 인혁아

--- issue/routes/uploadimage.js
+++ issue/routes/uploadimage.js
@@ -13,27 +13,51 @@ router.post('/', function(req, res){
 			res.send('plz send require elements');
 		}
 		let file_url = `${key}_${file.originalname}`;
-		console.log(file_url);
 		let query = ``;
+		let data_array =[];
 		switch(target){
 			case '부모님':
-				query = `UPDATE parentinfo SET cihld_image='${file_url}' WHERE id='${key}' AND child_name='${value1}';`
+				query = `UPDATE parentinfo SET cihld_image=? WHERE id=? AND child_name=?;`
+				data_array = [
+					file_url,
+					key,
+					value1
+				];
 				break;
 			case '식단표':
-				query = `UPDATE food_list SET food_image='${file_url}' WHERE school='${value1}' AND date='${key}';`
+				query = `UPDATE food_list SET food_image=? WHERE school=? AND date=?;`
+				data_array = [
+					file_url,
+					value1,
+					key
+				];
 				break;
 			case '선생님':
-				query = `UPDATE teachertinfo SET teacher_image='${file_url}' WHERE id='${key}';`
+				query = `UPDATE teachertinfo SET teacher_image=? WHERE id=?;`
+				data_array = [
+					file_url,
+					key
+				];
 				break;
 			case '원장님':
 				query = `UPDATE presidentinfo SET presi_image='${file_url}' WHERE id='${key}';`
+				data_array = [
+					file_url,
+					key
+				];
 				break;
 			case '앨범':
-				query = `UPDATE album SET album_image='${file_url}' WHERE title='${key}' AND school='${value1}' AND room='${value2}';`
+				query = `UPDATE album SET album_image=? WHERE title=? AND school=? AND room=?;`
+				data_array = [
+					file_url,
+					key,
+					value1,
+					value2
+				];
 				break;
 		}
-		console.log(query);
-		db.query(query, function(err, result){
+		console.log(query, data_array);
+		db.query(query, data_array, function(err, result){
 			if(err){
 				res.status(400).send(err);
 			}
