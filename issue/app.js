ac55ac081d8116303e039e6b7a1666fcf32304c4
wldlsgur <dhgg321@naver.com>
2022-04-13 00:51:39 +0900

    image test

--- issue/app.js
+++ issue/app.js
@@ -30,45 +30,7 @@ const storage = multer.diskStorage({
 		let key = req.params.key;
 		cb(null, `${key}_${file.originalname}`);
 	},
-	limits: {fileSize: 5 * 1024 * 1024},
-	update :function(req, res, file, cb){
-		let file = req.file;
-		let target = req.params.target;
-		let key = req.params.key;
-		let value1 = req.params.value1;
-		let value2 = req.params.value2;
-
-		if(!file || !target || !key || !value1 || !value2){
-			res.send('plz send require elements');
-		}
-		let file_url = `${key}_${file.originalname}`;
-		console.log(file_url);
-		let query = ``;
-		switch(target){
-			case '부모님':
-				query = `UPDATE parentinfo SET cihld_image='${file_url}' WHERE id='${key}' AND child_name='${value1}';`
-				break;
-			case '식단표':
-				query = `UPDATE food_list SET food_image='${file_url}' WHERE school='${value1}' AND date='${key}';`
-				break;
-			case '선생님':
-				query = `UPDATE teachertinfo SET teacher_image='${file_url}' WHERE id='${key}';`
-				break;
-			case '원장님':
-				query = `UPDATE presidentinfo SET presi_image='${file_url}' WHERE id='${key}';`
-				break;
-			case '앨범':
-				query = `UPDATE album SET album_image='${file_url}' WHERE title='${key}' AND school='${value1}' AND room='${value2}';`
-				break;
-		}
-		console.log(query);
-		db.query(query, function(err, result){
-			if(err){
-				res.status(400).send(err);
-			}
-			res.send({res : true, msg : 'success'});
-		})
-	}
+	limits: {fileSize: 5 * 1024 * 1024}
 })
 const upload = multer({ storage: storage })
 
@@ -105,7 +67,7 @@ app.use('/user', userRouter);
 app.use('/schoolmanagement', schoolmanagementRouter);
 app.use('/parentinfo', parentinfoRouter);
 app.use('/presidentinfo', presidentinfoRouter);
-app.use('/uploadimage/:target/:key/:value1/:value2', upload.single('image'), uploadimageRouter);
+app.use('/uploadimage', upload.single('image'), uploadimageRouter);
 
 //인승 추가(아래)
 app.use('/staff', staffRouter);
--- issue/routes/uploadimage.js
+++ issue/routes/uploadimage.js
@@ -2,7 +2,42 @@ const express = require('express');
 const router = express.Router();
 const db = require('../DB/db');
 
-router.post('/:target/:key/:value1/:value2', function(req, res){
-	res.send({res : true, msg : 'success'});
+router.post('/', function(req, res){
+		let file = req.file;
+		let target = req.body.target;
+		let key = req.body.key;
+		let value1 = req.body.value1;
+		let value2 = req.body.value2;
+
+		if(!file || !target || !key || !value1 || !value2){
+			res.send('plz send require elements');
+		}
+		let file_url = `${key}_${file.originalname}`;
+		console.log(file_url);
+		let query = ``;
+		switch(target){
+			case '부모님':
+				query = `UPDATE parentinfo SET cihld_image='${file_url}' WHERE id='${key}' AND child_name='${value1}';`
+				break;
+			case '식단표':
+				query = `UPDATE food_list SET food_image='${file_url}' WHERE school='${value1}' AND date='${key}';`
+				break;
+			case '선생님':
+				query = `UPDATE teachertinfo SET teacher_image='${file_url}' WHERE id='${key}';`
+				break;
+			case '원장님':
+				query = `UPDATE presidentinfo SET presi_image='${file_url}' WHERE id='${key}';`
+				break;
+			case '앨범':
+				query = `UPDATE album SET album_image='${file_url}' WHERE title='${key}' AND school='${value1}' AND room='${value2}';`
+				break;
+		}
+		console.log(query);
+		db.query(query, function(err, result){
+			if(err){
+				res.status(400).send(err);
+			}
+			res.send({res : true, msg : 'success'});
+		})
 });
 module.exports = router;
