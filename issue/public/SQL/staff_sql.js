9c1853351f641900b2fa71095ba471de63a567e4
JeongInSeung <wjddlstmd159@gmail.com>
2022-04-12 16:50:12 +0900

    staff 관련 라우터, sql 추가 err 전송 코드 추가 db_config 파일 이동

--- issue/public/SQL/staff_sql.js
+++ issue/public/SQL/staff_sql.js
@@ -1,28 +1,37 @@
-var pool = require('../../config/db_config');
+var pool = require('../../DB/db_config');
 
 module.exports = function () {
     return {
-        get_teacherinfo: function (school,callback) {
+        get_teacherinfo_use_school: function (school,callback) {
             pool.getConnection(function (err, con) {
                 var sql=`select * from teacherinfo where school='${school}'`;
                 con.query(sql,function(err,result,fields){
                     con.release();
-                    if(err) return;
+                    if(err) callback(err,null);
                     else callback(null,result);
                 })
             });
         },
-        get_presidentinfo: function (id,callback) {
+        get_teacherinfo_use_id: function (id,callback) {
             pool.getConnection(function (err, con) {
-                var sql=`select * from presidentinfo where id='${id}'`;
+                var sql=`select * from teacherinfo where id='${id}'`;
                 con.query(sql,function(err,result,fields){
                     con.release();
-                    if(err) return;
+                    if(err) callback(err,null);
                     else callback(null,result);
                 })
             })
         },
-
+        get_presidentinfo_use_id: function (id,callback) {
+            pool.getConnection(function (err, con) {
+                var sql=`select * from presidentinfo where id='${id}'`;
+                con.query(sql,function(err,result,fields){
+                    con.release();
+                    if(err) callback(err,null);
+                    else callback(null,result);
+                })
+            })
+        },
 
         pool: pool
     }
