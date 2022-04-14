2601614d0f2a128de47ae673342a03520fa45ac3
인혁 지 <dhgg321@naver.com>
2022-04-14 15:03:11 +0900

    same id 오류수정

--- issue/public/SQL/check_sql.js
+++ issue/public/SQL/check_sql.js
@@ -15,10 +15,10 @@ module.exports = function () {
         get_sameid_check : function(data, callback){
             pool.getConnection(function(err, con){
                 let get_sameid_check_query = `SELECT * FROM user WHERE id='${data[0]}'`;
-                con.query(get_sameid_check_query, function(err, result){
+                con.query(get_sameid_check_query, function(err,result,fields){
                     con.release();
                     if(err) callback(err, null);
-                    else callback(null, err);
+                    else callback(null, result);
                 })
             })
         },
