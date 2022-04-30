d27ed49ba882def54b43242b143f262bf2c17b8e
wldlsgur <dhgg321@naver.com>
2022-05-01 02:20:47 +0900

    test

--- issue/Function/make_query.js
+++ issue/Function/make_query.js
@@ -24,17 +24,19 @@ module.exports = {
         for(key of Object.keys(json_data)){//파싱해도 key는 Object 문자열을 넣을려면 이 반복문을 고정적으로 사용해야한다
             make_query += `${key},`;
         }
+        make_query = make_query.substring(0, make_query.length - 1);//마지막, 제거
+
         make_query += `FROM ${table} WHERE`;
 
         let or_and_cnt = 0;
-        for(var key in json_data){
-            make_query += `? `;
+        for(key of Object.keys(json_data)){
+            make_query += `${key}=? `;
             if(or_and_cnt != cnt){
                 make_query += or_and;
                 or_and_cnt++;
             }
         }
-        make_query = make_query.substring(0, make_query.length - 1);//마지막, 제거
+        make_query = make_query.substring(0, make_query.length - 1);//마지막 공백 제거
         make_query += `)`;
         console.log(make_query);
         return make_query;
