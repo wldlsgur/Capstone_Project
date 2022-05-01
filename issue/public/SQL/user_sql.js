309354fc8add97628c5c68f461a24ea327c58ca5
인혁 지 <dhgg321@naver.com>
2022-05-01 19:47:14 +0900

    tsets

---
+++ issue/public/SQL/user_sql.js
@@ -0,0 +1,16 @@
+var pool = require('../../DB/db_config');
+
+module.exports = function () {
+    return {
+        SELECT : function (query, data_array, callback) {
+            pool.getConnection(function (err, con) {
+                con.query(query, data_array, function(err,result,fields){
+                    con.release();
+                    if(err) callback(err,null);
+                    else callback(null,result);
+                })
+            });
+        },
+        pool: pool
+    }
+};
