4b7c15b568980419fe408d325d1a2e70d4870784
JeongInSeung <wjddlstmd159@gmail.com>
2022-05-10 02:08:45 +0900

    오류수정

--- issue/public/SQL/test_sql.js
+++ issue/public/SQL/test_sql.js
@@ -4,7 +4,7 @@ module.exports = function () {
     return {
         test: function (id, school, room, number, callback) {
                                   pool.getConnection(function (err, con) {
-                                  let sql =`insert into presidentinfo (id, school, room, number, image_url) values('${id}','${school}','${room}','${number}',DATE_ADD(NOW(), INTERVAL 1 DAY);)`;
+                                  let sql =`insert into presidentinfo (id, school, room, number, image_url) values('${id}','${school}','${room}','${number}',DATE_ADD(NOW(), INTERVAL 1 DAY))`;
                                       con.query(sql,function(err,result,fields){
                                           con.release();
                                           if(err) callback(err,null);
