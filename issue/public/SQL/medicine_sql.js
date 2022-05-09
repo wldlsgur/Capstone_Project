8f6b329f95281b1a3149d51b49ee6e0a490e5470
JeongInSeung <wjddlstmd159@gmail.com>
2022-05-10 01:27:15 +0900

    test2

--- issue/public/SQL/medicine_sql.js
+++ issue/public/SQL/medicine_sql.js
@@ -4,11 +4,12 @@ module.exports = function () {
     return {
         insertMedicineInfoAll: function (id, child_name, m_name, morning, lunch, dinner, date, mPlace, content, school, room,callback) {
                     pool.getConnection(function (err, con) {
-                        let insertMedicineInfo = `insert into medicine ('id', 'child_name', 'm_name', 'morning', 'lunch', 'dinner', 'date', 'mPlace', 'content')
-                                                  values('${id}','${child_name}','${m_name}','${morning}','${lunch}','${dinner}','${date}','${mPlace}','${content}')`;
-                        let insertMedicineManagementInfo = `insert into medicinemanagement ('id', 'school', 'room', 'child_name', 'm_name', 'date', 'morning', 'lunch', 'dinner')
-                                                            values('${id}', '${school}', '${room}', '${child_name}','${m_name}','${NOW()}','false','false','false')`;
-                        let multiQuery = `${insertMedicineInfo};${insertMedicineManagementInfo};`
+                        let insertMedicineInfo = `insert into medicine (id, child_name, m_name, morning, lunch, dinner, date, mPlace, content)
+                                                  values('${id}','${child_name}','${m_name}','${morning}','${lunch}','${dinner}','${date}','${mPlace}','${content}');`;
+                        let insertMedicineManagementInfo = `insert into medicinemanagement (id, school, room, child_name, m_name, date, morning, lunch, dinner)
+                                                            values('${id}', '${school}', '${room}', '${child_name}','${m_name}',NOW(),'false','false','false');`;
+                        let multiQuery = insertMedicineInfo+insertMedicineManagementInfo;
+                        console.log(multiQuery);
                         con.query(multiQuery,function(err,result,fields){
                             con.release();
                             if(err) callback(err,null);
--- issue/public/SQL/test_sql.js
+++ issue/public/SQL/test_sql.js
@@ -4,7 +4,7 @@ module.exports = function () {
     return {
         test: function (id, school, room, number, callback) {
                                   pool.getConnection(function (err, con) {
-                                  let sql =`insert into presidentinfo (id, school, room, number, image_url) values('${id}','${school}','${room}','${number}',NOW())`;
+                                  let sql =`insert into presidentinfo (id, school, room, number, image_url) values('${id}','${school}','${room}','${number}',NOW()+1)`;
                                       con.query(sql,function(err,result,fields){
                                           con.release();
                                           if(err) callback(err,null);
