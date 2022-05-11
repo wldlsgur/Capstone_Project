a3f6b0fb98a248c68d4f81ebf5d76e94dee35667
wldlsgur <dhgg321@naver.com>
2022-05-12 01:43:02 +0900

    promise 정복중

--- issue/routes/album.js
+++ issue/routes/album.js
@@ -15,75 +15,60 @@ router.get('/info', function(req, res){
         res.send(element_msg);
         return;
     }
-    
+
+    let array1 = {};
     let array2 = [];
     let query1 = `SELECT distinct title, date FROM album WHERE school='${json_data.school}' AND room='${json_data.room}'`;
-    db_album_sql.SELECT(query1, function(err, result1){
-        if(err){
-            res.status(400).send(err);
-            return;
-        }
-        for(let i=0 ; i<result1.length ; i++){
-            let array1 = {};
-            array1.title = result1[i].title;
-            array1.date = result1[i].date;
-            let query2 = `SELECT image_url FROM album WHERE school='${json_data.school}' AND room='${json_data.room}' AND title = '${result1[i].title}' AND date='${result1[i].date}'`;
+    function job1 (){
+        return new Promise(function(resolve, rejected){
+            db_album_sql.SELECT(query1, function(err, result){
+                if(err){
+                    rejected(err);
+                }
+                resolve(result);
+            })
+        })
+    }          
+    function job2 (query2){
+        return new Promise(function(resolve, rejected){
+            db_album_sql.SELECT(query2, function(err, result){
+                if(err){
+                    rejected(err);
+                }
+                resolve(result);
+            })
+        })
+    }
+    job1()
+    .then(function(result){
+        for(let i=0 ; i<result.length ; i++){
+            array1 = {};
+            array1.title = result[i].title;
+            array1.date = result[i].date;
+            let query2 = `SELECT image_url FROM album WHERE school='${json_data.school}' AND room='${json_data.room}' AND title = '${result[i].title}' AND date='${result[i].date}'`;
             console.log(`첫번째 : ${i}`, array1);
-            
-            function job1 (){
-                return new Promise(function(resolve, rejected){
-                    db_album_sql.SELECT(query2, function(err, result){
-                        if(err){
-                            rejected(err);
-                        }
-                        resolve(result);
-                    })
-                })
-            }
-            job1()
-            .then(function(result){
-                    let img_url = [];
-                    for(let j =0 ; j < result.length; j++)
-                    {
-                        img_url.push(result[j].image_url);
-                    }
-                    array1.image_url = img_url;
-                    console.log(`두번째`, array1);
-                    array2.push(array1);
-            })
-            .catch(function(err){
-                console.log(err)
-                res.status(400).send(err);
-            })
+
+            return job2(query2);
         }
+    })
+    .then(function(result){
+        let img_url = [];
+        for(let j =0 ; j < result.length; j++)
+        {
+            img_url.push(result[j].image_url);
+        }
+        array1.image_url = img_url;
+        console.log(`두번째`, array1);
+        array2.push(array1);
+        return;
+    })
+    .then(function(){
         console.log("최종",array2);
         res.status(200).send(array2);
     })
-})
-
-router.get('/image_url', function(req, res){
-    let json_data = {
-        school : req.query.school,
-        room : req.query.room,
-        title : req.query.title,
-        date : req.query.date
-    }
-    let target_array = [
-        'image_url'
-    ]
-    
-    if(check_element.check_require_element(json_data) === false){
-        res.send(element_msg);
-        return;
-    }
-
-    let query = make_query.SELECT(target_array, 'album', json_data, 'AND', 2);
-    db_album_sql.SELECT(query, function(err, result){
-        if(err){
-            res.status(400).send(err);
-        }
-        res.send(result);
+    .catch(function(err){
+        console.log(err)
+        res.status(400).send(err);
     })
 })
-
 module.exports = router;
