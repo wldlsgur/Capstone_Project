354cdd51dbb3cb1e05043f5253b365f01fd75741
JeongInSeung <wjddlstmd159@gmail.com>
2022-05-18 01:57:36 +0900

    약 정보 전송 API 및 쿼리 추가

--- issue/public/SQL/medicine_sql.js
+++ issue/public/SQL/medicine_sql.js
@@ -27,18 +27,31 @@ module.exports = function () {
                     })
                 },
         selectMedicinemanageInfo: function (school,room,callback) {
-                            pool.getConnection(function (err, con) {
-                                let sql=`SELECT mm.id, mm.child_name, mm.m_name, mm.date, mm.mor, mm.lun, mm.din, m.morning, m.lunch, m.dinner 
-                                         from medicinemanagement AS mm
-                                         LEFT JOIN medicine AS m
-                                         ON mm.school='${school}' AND mm.room='${room}' AND mm.id=m.id AND mm.child_name=m.child_name AND mm.m_name=m.m_name`;
-                                con.query(sql,function(err,result,fields){
-                                    con.release();
-                                    if(err) callback(err,null);
-                                    else callback(null,result);
-                                })
-                            })
-                        },
+                    pool.getConnection(function (err, con) {
+                        let sql=`SELECT mm.id, mm.child_name, mm.m_name, mm.date, mm.mor, mm.lun, mm.din, m.morning, m.lunch, m.dinner 
+                                from medicinemanagement AS mm
+                                LEFT JOIN medicine AS m
+                                ON mm.school='${school}' AND mm.room='${room}' AND mm.id=m.id AND mm.child_name=m.child_name AND mm.m_name=m.m_name`;
+                            con.query(sql,function(err,result,fields){
+                                con.release();
+                                if(err) callback(err,null);
+                                else callback(null,result);
+                        })
+                    })
+                },
+        selectMedicinemanageInfo_useId_chNm: function (id,child_name,callback) {
+                    pool.getConnection(function (err, con) {
+                        let sql=`SELECT mm.id, mm.child_name, mm.m_name, mm.date, mm.mor, mm.lun, mm.din, m.morning, m.lunch, m.dinner 
+                                 from medicinemanagement AS mm
+                                 LEFT JOIN medicine AS m
+                                 ON mm.id='${id}' AND mm.child_name='${child_name}' AND mm.id=m.id AND mm.child_name=m.child_name AND mm.m_name=m.m_name`;
+                            con.query(sql,function(err,result,fields){
+                                con.release();
+                                if(err) callback(err,null);
+                                else callback(null,result);
+                        })
+                    })
+                },
 
         pool: pool
     }
