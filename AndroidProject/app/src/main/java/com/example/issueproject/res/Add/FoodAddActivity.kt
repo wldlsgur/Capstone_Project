472a40ee389af93e93a7741c1b4c30bce01a3b37
LeeBoyeon <61018094+leeboyeon@users.noreply.github.com>
2022-05-27 21:25:21 +0900

    Custom Layout Complete

---
+++ AndroidProject/app/src/main/java/com/example/issueproject/res/Add/FoodAddActivity.kt
@@ -0,0 +1,12 @@
+package com.example.issueproject.res.Add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.issueproject.R
+
+class FoodAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_add)
+    }
+}
