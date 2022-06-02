d0e8e52e2a33f4075447c6527364b2c4a85aba12
인혁 지 <dhgg321@naver.com>
2022-06-02 15:22:32 +0900

    ci cd test 28

--- issue/routes/index.js
+++ issue/routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router ();
 
 router.get('/', function(req, res, next){
-  res.send("무중단test27!!")  
+  res.send("무중단test28!!")  
 });
 module.exports = router;
