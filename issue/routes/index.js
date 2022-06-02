a529f232937be962aa397be25bea32bb8601a8ab
wldlsgur <dhgg321@naver.com>
2022-06-02 19:38:33 +0900

    ci cd test 40!!

--- issue/routes/index.js
+++ issue/routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router ();
 
 router.get('/', function(req, res, next){
-  res.send("무중단test39!!")  
+  res.send("무중단test40!!")  
 });
 module.exports = router;
