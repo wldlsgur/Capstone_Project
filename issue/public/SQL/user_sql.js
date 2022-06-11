b1896a0e3228f3969a8edbc2027817a72e3387bb
wldlsgur <dhgg321@naver.com>
2022-06-11 21:38:47 +0900

    비밀번호 변경 api 오류 수정

--- issue/public/SQL/user_sql.js
+++ issue/public/SQL/user_sql.js
@@ -20,6 +20,15 @@ module.exports = function () {
                 })
             })
         },
+        UPDATE : function(query, callback){
+            pool.getConnection(function(err, con){
+                con.query(query, function(err, result, fields){
+                    con.release();
+                    if(err) callback(err,null);
+                    else callback(null,result);
+                })
+            })
+        },
         pool: pool
     }
 };
